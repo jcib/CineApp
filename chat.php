ad39870d39e9f923f4966bfcdb4f7e50dc02a16d
jcib <a18josibacuc@iam.cat>
2019-09-10 01:33:08 +0200

    update

---
+++ chat.php
@@ -0,0 +1,11 @@
+<?php include "templates/header.php"; ?>
+
+<div id="contenedor">
+    <div id="chat">
+    <h4>SALA DE CHAT</h4>
+    <iframe src="https://www5.cbox.ws/box/?boxid=915369&boxtag=lAroku" width="450px" height="450px" allowtransparency="yes" allow="autoplay" frameborder="0" marginheight="0" marginwidth="0" scrolling="auto"></iframe>	
+    </div>
+    <a href="index.php">Volver atrás</a>
+</div>
+
+<?php include "templates/footer.php"; ?>
--- index.php
+++ index.php
@@ -25,13 +25,19 @@
     <li>
       <a href="delete.php"><strong>Borrar películas</strong></a>
     </li>
+    <li>
+      <a href="chat.php"><strong>Chat</strong></a>
+    </li>
   </ul>
 
   <div id="imagen">
     <img id ="imagen1" src="https://media.giphy.com/media/GAQmPS2LbaEXm/giphy.gif" alt="12 hombres sin piedad" />
+    <p></p>
+    <a href="//24timezones.com/es_husohorario/barcelona_hora_actual.php" style="text-decoration: none" class="clock24" id="tz24-1568071699-c131-eyJob3VydHlwZSI6MTIsInNob3dkYXRlIjoiMSIsInNob3dzZWNvbmRzIjoiMCIsInNob3d0aW1lem9uZSI6IjAiLCJ0eXBlIjoiZCIsImxhbmciOiJlcyJ9" title="Reloj Mundial - Barcelona" target="_blank" rel="nofollow">Barcelona</a>
+    <script type="text/javascript" src="//w.24timezones.com/l.js" async></script>
   </div>
   <h5 id="firma">Creado por José Carlos Ibarra</h5>
+  <h6 id="firma">versión - 0.0.1</h6>
 </div>
-
 
 <?php include "templates/footer.php"; ?>
