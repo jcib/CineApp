9a8957d689088f3a5c9f4bd60a08deebd9187d31
jcib <a18josibacuc@iam.cat>
2019-09-10 11:18:13 +0200

    added index file

--- templates/header.php
+++ templates/header.php
@@ -5,7 +5,7 @@
     <meta http-equiv="x-ua-compatible" content="ie=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-    <title>El rincón cinéfilo</title>
+    <title>El rincón del cinéfilo</title>
 
     <link rel="stylesheet" href="css/style.css" />
     <link rel="icon" 
